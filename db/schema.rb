f11e086f94c71307e6b6e055abf7c793520f8040
KamiyamaShun <76637787+KamiyamaShun@users.noreply.github.com>
2020-12-30 12:17:35 +0900

    ユーザー編集機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_30_003147) do
+ActiveRecord::Schema.define(version: 2020_12_30_024752) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
